0a3d825ebe2e67d7985a8ab952a8767800031a0c
Devamitta <rusdevamitta@gmail.com>
2022-06-16 13:53:22 +0800

    update

--- clean.py
+++ clean.py
@@ -49,7 +49,7 @@ with open(f"patimokkhaSBS.txt", 'r') as input_file :
 	
 clean_text = clean_machine(text)
 
-with open(f"clean_patimokka.txt", 'w') as save_file :
+with open(f"clean_patSBS.txt", 'w') as save_file :
 	save_file.write(clean_text)
 
 input_file.close()
@@ -60,8 +60,19 @@ with open(f"ptmk_sentences.txt", 'r') as ptmk_ods :
 	
 ptmk_ods_text_clean = clean_machine(ptmk_ods_text)
 
-with open(f"clean_patimokka_ods.txt", 'w') as ptmk_ods_save :
+with open(f"clean_patDVE.txt", 'w') as ptmk_ods_save :
 	ptmk_ods_save.write(ptmk_ods_text_clean)
 
 ptmk_ods.close()
 ptmk_ods_save.close()
+
+with open(f"patTHAI.txt", 'r') as ptmk_THAI :
+	ptmk_THAI_text = ptmk_THAI.read()
+	
+ptmk_THAI_text_clean = clean_machine(ptmk_THAI_text)
+
+with open(f"clean_patTHAI.txt", 'w') as ptmk_THAI_save :
+	ptmk_THAI_save.write(ptmk_THAI_text_clean)
+
+ptmk_THAI.close()
+ptmk_THAI_save.close()

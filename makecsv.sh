276cdbae597e35d651502139bb6e5732a74441ae
Devamitta <rusdevamitta@gmail.com>
2022-07-09 00:01:17 +0800

    update

--- makecsv.sh
+++ makecsv.sh
@@ -6,11 +6,11 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 # python3 ods_to_csv.py "Pātimokkha Word by Word.ods" Sheet1 20
 
-cp "original_sources/Pātimokkha Word by Word.ods" "Pātimokkha Word by Word.ods"
+mv "original_sources/Pātimokkha Word by Word.ods" "Pātimokkha Word by Word.ods"
 
 python3 ods-to-csv-headers.py "Pātimokkha Word by Word.ods" Sheet1 20
 
-# mv "Pātimokkha Word by Word.csv" "original_sources/Pātimokkha Word by Word.csv"
+mv "Pātimokkha Word by Word.ods" "original_sources/Pātimokkha Word by Word.ods"
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "filtering words that have been done"

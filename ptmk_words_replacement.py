0f4444958710e9266e54d2ada7a3d182fa1bea2c
Kohoshiro <kohoshiro@blackfire.realtek>
2022-07-10 15:09:09 +0800

    inflection vs pat sbs text replacement

--- ptmk_words_replacement.py
+++ ptmk_words_replacement.py
@@ -1,23 +1,15 @@
 import pandas as pd
 import csv
+import re
 
 
 def read_differences_file() -> dict:
     df = pd.read_csv("original_sources/differences.csv")
     return df.to_dict(orient="records")
 
-
-def replace_pat_sbs_file():
-    diff_dict = read_differences_file()
-    
-    with open("original_sources/PAT_SBS.txt", "r") as pat_sbs_file:
-        pat_sbs_data = pat_sbs_file.read()
-
-    for word in diff_dict:
-        pat_sbs_data = pat_sbs_data.replace(word["now"], word["result"])
-
-    with open("curated_sources/PAT_SBS_REPLACED.txt", "w") as pat_sbs_replaced_file:
-        pat_sbs_replaced_file.write(pat_sbs_data)
+def read_inflections_file() -> dict:
+    df = pd.read_csv("curated_sources/all_inflections_modified.csv")
+    return df.to_dict(orient="records")
 
 
 def replace_pat_ods_file():
@@ -35,19 +27,51 @@ def replace_inflections_file():
     data = df.to_dict(orient="records")
 
     csvheader = ["inflection", "headwords"]
-    with open("frequency/all_inflections_modified.csv", "w") as csvwriter:
-        writer = csv.writer(csvwriter, delimiter="\t")
+    with open("curated_sources/all_inflections_modified.csv", "w") as csvwriter:
+        writer = csv.writer(csvwriter, delimiter=",")
         writer.writerow(csvheader)
         for row in data:
-            non_numeric_str = ''.join([i for i in row["headwords"] if not i.isdigit()])
-            non_numeric_str = non_numeric_str.replace(" ", "").replace("[", "").replace("]", "").replace("'", "")
-            headwords_list = list(non_numeric_str.split(","))
+            #process inflections
+            row["inflection"] = row["inflection"].replace(" ", "")
+            #process headwords
+            headwords_non_numeric_str = "".join([i for i in row["headwords"] if not i.isdigit()])
+            headwords_non_numeric_str = headwords_non_numeric_str.replace(" ", "").replace("[", "").replace("]", "").replace("'", "")
+            headwords_list = list(headwords_non_numeric_str.split(","))
             headwords_list = pd.unique(headwords_list).tolist()
-            row["headwords"] = ','.join(map(str, headwords_list))
+            row["headwords"] = " ".join(map(str, headwords_list))
             csvrow = [row["inflection"], row["headwords"]]
             writer.writerow(csvrow)
+    replace_pat_sbs_file()
+    replace_pat_ods_file()
 
 
-replace_pat_sbs_file()
-replace_pat_ods_file()
+def replace_pat_sbs_file():
+    diff_dict = read_differences_file()
+    inflections_dict = read_inflections_file()
+    
+    with open("original_sources/PAT_SBS.txt", "r") as pat_sbs_file:
+        pat_sbs_data = pat_sbs_file.read()
+
+    for word in diff_dict:
+        pat_sbs_data = pat_sbs_data.replace(word["now"], word["result"])
+
+    pat_sbs_data = pat_sbs_data.replace("\n\n", " \n\n ").replace(".", " . ").replace(":", " : ")
+    pat_sbs_data = pat_sbs_data.replace("?", " ? ").replace(",", " , ").replace("'", " ' ").replace("`", " ` ")
+    pat_sbs_data_list = list(pat_sbs_data.split(" "))
+
+    for inflection_words in inflections_dict:
+        for pat_sbs_data_word in pat_sbs_data_list:
+            if inflection_words["inflection"] == pat_sbs_data_word.lower():
+                index = pat_sbs_data_list.index(pat_sbs_data_word)
+                pat_sbs_data_list[index] = inflection_words["headwords"]
+                print(f"{pat_sbs_data_word} -> {pat_sbs_data_list[index]} REPLACED!")
+
+    pat_sbs_data = " ".join(pat_sbs_data_list)
+    pat_sbs_data = pat_sbs_data.replace(" \n\n ", "\n\n").replace(" . ", ".").replace(" : ", ":")
+    pat_sbs_data = pat_sbs_data.replace(" ? ", "?").replace(" , ", ",").replace(" ' ", "'").replace(" ` ", "`")
+
+    with open("curated_sources/PAT_SBS_REPLACED.txt", "w") as pat_sbs_replaced_file:
+        pat_sbs_replaced_file.write(pat_sbs_data)
+
+
 replace_inflections_file()

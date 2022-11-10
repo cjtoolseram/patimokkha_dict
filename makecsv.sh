dd22a04ff62a5f51cad453791968d661b8a34aad
Devamitta <rusdevamitta@gmail.com>
2022-11-10 19:01:54 +0800

    clean

--- makecsv.sh
+++ makecsv.sh
@@ -1,16 +1,14 @@
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "moving Patimokkha ods from Downloads"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+mv "/home/deva/Downloads/Pātimokkha Word by Word.ods" "/home/deva/Documents/dps/patimokkha_dict/original_sources/Pātimokkha Word by Word.ods"
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "making Patimokkha csv with bold - ods-to-csv-headers.py"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-#cd "/home/deva/Documents/sasanarakkha/patimokkha-analysis/original_sources"
-
-# python3 ods_to_csv.py "Pātimokkha Word by Word.ods" Sheet1 20
-
-# mv "original_sources/Pātimokkha Word by Word.ods" "Pātimokkha Word by Word.ods"
-
 python3 ods-to-csv-headers.py "original_sources/Pātimokkha Word by Word.ods" Sheet1 20
-
-# mv "Pātimokkha Word by Word.ods" "original_sources/Pātimokkha Word by Word.ods"
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "filtering words that have been done"
--- patimokkha filter.py
+++ patimokkha filter.py
@@ -13,14 +13,14 @@ df = df.loc[filter]
 # change tamil
 test1 = df['meaning'] != ""
 filter = test1
-df.loc[filter, ['tamil']] = ""
+df.loc[filter, ['native']] = ""
 
 # choosing order of columns
-df = df[['bhikkhupātimokkhapāḷi', 'pos', 'grammar', '+case', 'tamil', 
+df = df[['pali', 'pos', 'grammar', '+case', 'native', 
         'meaning', 'lit. meaning', 'root', 'rt gp', 'sign', 'base', 
         'construction', 'compound type', 'compound construction', 'abbrev', 
-        'source', 'sentence', 'aṭṭhakathā']]
-df.insert(18, 'feedback', "<a href=\"https://docs.google.com/forms/d/e/1FAIpQLSdG6zKDtlwibtrX-cbKVn4WmIs8miH4VnuJvb7f94plCDKJyA/viewform?usp=pp_url&entry.438735500="+df['bhikkhupātimokkhapāḷi']+"\">feedback</a>")    
+        'source', 'sentence', 'aṭṭhakathā', 'test number']]
+df.insert(17, 'feedback', "<a href=\"https://docs.google.com/forms/d/e/1FAIpQLSdG6zKDtlwibtrX-cbKVn4WmIs8miH4VnuJvb7f94plCDKJyA/viewform?usp=pp_url&entry.438735500="+df['bhikkhupātimokkhapāḷi']+"\">feedback</a>")    
 
 # save csv
 df.to_csv("curated_sources/Pātimokkha Word by Word.csv", sep="\t", index=None)
